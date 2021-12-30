2dfe78a1b6f0a5e831b540c3667207909e42d5cc
orionmiz <x3ph3r@naver.com>
2021-12-31 02:07:35 +0900

    solve: [11] Container With Most Water

--- 11.container-with-most-water.ts
+++ 11.container-with-most-water.ts
@@ -7,20 +7,22 @@
 // @lc code=start
 function maxArea(height: number[]): number {
   let answer = 0;
-  for (let i = 0; i < height.length; i++) {
+  const len = height.length;
+  for (let i = 0; i < len; i++) {
     const h1 = height[i];
-    for (let j = i + 1; j < height.length; j++) {
+    if (answer > h1 * (len - i - 1)) // max area
+      continue;
+    for (let j = len - 1; j > i; j--) {
       const h2 = height[j];
-      if (h2 >= h1) {
-        const area = (j - i) * h1;
-        if (area > answer) {
-          answer = area;
-        }
+      if (h2 >= h1) { // found
+        answer =  h1 * (j - i);
+        break;
       } else { // h2 < h1
-        const area = (j - i) * h2;
-        if (area > answer) {
+        const area = h2 * (j - i);
+        if (area > answer)
           answer = area;
-        }
+        if (answer > h1 * (j - i - 1)) // max area
+          break;
       }
     }
   }
@@ -29,7 +31,7 @@ function maxArea(height: number[]): number {
 // @lc code=end
 
 // [time limit problem]
-// loop j from back & skip j with width
+// loop j from back & skip j with width (check max area)
 // => this is better. because max n is bigger than max height
 // or
 // loop j from front & skip each index
